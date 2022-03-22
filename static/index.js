1515f5d65a40e4e51da83e28c0a95b4fe3cbddf7
nemo <67428464+cuichanger@users.noreply.github.com>
2022-03-22 14:43:47 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -73,7 +73,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
     }
 
     function modeToString(m) {
-        return m === MODE_NORMAL ? "普通模式" : (m === MODE_ENDLESS ? "无尽模式" : "练习模式");
+        return m === MODE_NORMAL ? "小可模式" : (m === MODE_ENDLESS ? "kero模式" : "红黑毛模式");
     }
 
     w.changeMode = function(m) {
